e77de07474a36b9f5306274ad1982e133ec825e6
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-10-20 00:17:15 -0700

    Add Todo class

---
+++ src/todo.js
@@ -0,0 +1,24 @@
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    setTitle(title) {
+        this.title = title;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    setDueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    setPriority(priority) {
+        this.priority = priority;
+    }
+}
