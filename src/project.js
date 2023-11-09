0174d2a05397c16ecce1941912e4d5fec1ae1eb6
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-11-09 00:01:38 -0800

    Clean code

--- src/project.js
+++ src/project.js
@@ -1,32 +1,119 @@
-export { projectList, addProject, deleteProject };
-import { Todo } from './todo.js';
+export { ProjectList };
 
-let projectList = [];
+class ProjectList {
+    constructor() {
+        this.activeProjectIndex = null;
+        this.projectList = [];
+    }
+
+    setActiveProjectIndex(newActiveProjectIndex) {
+        this.activeProjectIndex = newActiveProjectIndex;
+    }
+
+    // Add and delete projects and tasks
+    addProject(title) {
+        this.projectList.push(new Project(title));
+    }
+
+    addTask(title, description, dueDate, priority) {
+        this.projectList[this.activeProjectIndex].taskList.push(new Task(title, description, dueDate, priority));
+    }
+
+    deleteProject(projectIndex) {
+        this.projectList.splice(projectIndex, 1);
+    }
+
+    deleteTask(projectIndex, taskIndex) {
+        this.projectList[projectIndex].taskList.splice(taskIndex, 1);
+    }
+
+    // Set projects and tasks
+    setProjectTitle(title, projectIndex) {
+        this.projectList[projectIndex].title = title;
+    }
+
+    setTaskTitle(title, taskIndex) {
+        this.projectList[this.activeProjectIndex].taskList[taskIndex].title = title;
+    }
+
+    setTaskDescription(description, taskIndex) {
+        this.projectList[this.activeProjectIndex].taskList[taskIndex].description = description;
+    }
+
+    setTaskDueDate(dueDate, taskIndex) {
+        this.projectList[this.activeProjectIndex].taskList[taskIndex].dueDate = dueDate;
+    }
+    
+    setTaskPriority(priority, taskIndex) {
+        this.projectList[this.activeProjectIndex].taskList[taskIndex].priority = priority;
+    }
+}
 
 class Project {
     constructor(title) {
         this.title = title;
-        this.todoList = [];
+        this.taskList = [];
     }
+}
 
-    setTitle(title) {
+class Task {
+    constructor(title, description, dueDate, priority) {
         this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
     }
-
-    addTodo(title, description, dueDate, priority) {
-        this.todoList.push(new Todo(title, description, dueDate, priority));
-    }
-
-    deleteTodo(todoIndex) {
-        delete this.todoList.splice(todoIndex, 1);
-    }
-
 }
 
-function addProject(title) {
-    projectList.push(new Project(title));
-}
 
-function deleteProject(projectIndex) {
-    projectList.splice(projectIndex, 1);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export { projectList, addProject, deleteProject };
+// import { Todo } from './todo.js';
+
+// let projectList = [];
+
+// class Project {
+//     constructor(title) {
+//         this.title = title;
+//         this.todoList = [];
+//     }
+
+//     setTitle(title) {
+//         this.title = title;
+//     }
+
+//     addTodo(title, description, dueDate, priority) {
+//         this.todoList.push(new Todo(title, description, dueDate, priority));
+//     }
+
+//     deleteTodo(todoIndex) {
+//         delete this.todoList.splice(todoIndex, 1);
+//     }
+
+// }
+
+// function addProject(title) {
+//     projectList.push(new Project(title));
+// }
+
+// function deleteProject(projectIndex) {
+//     projectList.splice(projectIndex, 1);
+// }
