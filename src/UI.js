9107b4c5cefde13831505c621eea9933aac83994
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-11-09 22:49:18 -0800

    Clean code

--- src/UI.js
+++ src/UI.js
@@ -1,13 +1,100 @@
-export { updateDOM };
-import { projectList } from './index.js';
+export { updateScreen };
+import { projects } from './index.js';
 
-function updateDOM() {
-    console.log(projectList);
+
+// Update screen
+function updateScreen() {
+    console.log(projects);
+    appendProjectDOM();
+    if (projects.activeProjectIndex !== null) appendTaskDOM();
+}
+
+// Add projects to navbar
+function appendProjectDOM() {
+    const navBar = document.querySelector('.project-list');
+    navBar.textContent = '';
+
+    for (let i = 0; i < projects.projectList.length; i++) {
+        const project = document.createElement('div');
+
+        // Create project button
+        const projectTitle = document.createElement('button');
+        projectTitle.textContent = projects.projectList[i].title;
+        projectTitle.addEventListener('click', () => {
+            projects.setActiveProjectIndex(i);
+            updateScreen();
+        });
+
+        // Create delete button
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '🗑️';
+        deleteButton.addEventListener('click', () => {
+            projects.deleteProject(i);
+            updateScreen();
+        });
+
+        // Append DOM
+        navBar.appendChild(project);
+        project.appendChild(projectTitle);
+        project.appendChild(deleteButton);
+    }
 }
 
 
+// Add tasks to content
+function appendTaskDOM() {
+    const content = document.querySelector('.content');
+    content.textContent = '';
 
+    // Create project title
+    const projectTitle = document.createElement('h2');
+    projectTitle.textContent = projects.projectList[projects.activeProjectIndex].title;
+    content.appendChild(projectTitle);
 
+    for (let i = 0; i < projects.projectList[projects.activeProjectIndex].taskList.length; i++) {
+        const task = document.createElement('div');
+        const taskElements = document.createElement('div');
+        const taskActions = document.createElement('div');
+
+        // Create task elements
+        const taskTitle = document.createElement('p');
+        taskTitle.textContent = projects.projectList[projects.activeProjectIndex].taskList[i].title;
+        
+        const taskDescription = document.createElement('p');
+        taskDescription.textContent = projects.projectList[projects.activeProjectIndex].taskList[i].description;
+
+        const taskDueDate = document.createElement('p');
+        taskDueDate.textContent = projects.projectList[projects.activeProjectIndex].taskList[i].dueDate;
+
+        const taskPriority = document.createElement('p');
+        taskPriority.textContent = projects.projectList[projects.activeProjectIndex].taskList[i].priority;
+
+        // Create task actions
+        const editButton = document.createElement('button');
+        editButton.textContent = '✎';
+        editButton.addEventListener('click', () => {
+
+        });
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '🗑️';
+        deleteButton.addEventListener('click', () => {
+            projects.deleteTask(projects.activeProjectIndex, i);
+            updateScreen();
+        });
+
+        // Append DOM
+        content.appendChild(task);
+        task.appendChild(taskElements);
+        task.appendChild(taskActions);
+        taskElements.appendChild(taskTitle);
+        taskElements.appendChild(taskDescription);
+        taskElements.appendChild(taskDueDate);
+        taskElements.appendChild(taskPriority);
+        taskActions.appendChild(editButton);
+        taskActions.appendChild(deleteButton);
+    }
+}
 
 
 
